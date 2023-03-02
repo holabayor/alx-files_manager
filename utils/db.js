2e30bbe894e441b2b1ff1e53d137fba29f8bc00f
holabayor <liasu.olabayo@gmail.com>
2023-03-02 10:20:32 +0100

    fix naming

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,5 @@
 const DBClient = class {
-  constructor() {
-    this.host = 'localhost';
-    this.port = 27017;
-    this.database = 'files_manager';
-  }
+
 };
 
 const dbClient = new DBClient();
---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+const RedisClient = class {
+  constructor() {
+    this.client = redis.createClient();
+    this.isConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Failed to connect: ', err);
+      this.isConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    const value = promisify(this.client.GET).bind(this.client)(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+};
+
+export default new RedisClient();
