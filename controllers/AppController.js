fc721a1ab7fd6112a74882c2140494aba2fed938
holabayor <liasu.olabayo@gmail.com>
2023-03-04 00:46:33 +0100

    some fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,14 +2,14 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(res, req) {
+  static getStatus(_res, req) {
     req.status(200).send({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     });
   }
 
-  static getStats(res, req) {
+  static getStats(_res, req) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([users, files]) => {
         req.status(200).send({
