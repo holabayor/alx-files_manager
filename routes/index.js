e4e77b2e41e45520d9a52a72299f700b0377fb16
holabayor <liasu.olabayo@gmail.com>
2023-03-04 21:18:30 +0100

    index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = Router();
+const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
