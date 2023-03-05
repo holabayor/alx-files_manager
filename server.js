cb600d717d2cabfc14421a2f87acc350cc3b62db
holabayor <liasu.olabayo@gmail.com>
2023-03-05 13:49:06 +0100

    routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes/index';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
