fcd1c9469d8175d72788f930d9f6972edd053842
holabayor <liasu.olabayo@gmail.com>
2023-03-07 19:22:42 +0100

    index file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -223,6 +223,37 @@ class FilesController {
     delete newFile.localPath;
     res.status(200).send(newFile);
   }
+
+  static async getFile(req, res) {
+    const token = req.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    const user = await dbClient.db.collection('users')
+      .findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    const { id } = req.params;
+    const file = await dbClient.db.collection('files')
+      .findOne({ _id: ObjectId(id), userId: user._id });
+    if (!file) {
+      res.status(404).send({ error: 'Not found' });
+      return;
+    }
+    const newFile = {
+      id: file._id,
+      ...file,
+    };
+    newFile.isPublic = false;
+    delete newFile._id;
+    delete newFile.localPath;
+    res.status(200).send(newFile);
+  }
 }
 
 export default FilesController;
